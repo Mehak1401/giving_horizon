378b560de85f8d1fa8de32ee659f6126786ef5d9
Anurag Goel <gtg07817@gmail.com>
2025-02-04 16:57:52 +0530

    Database and Backend setup

--- database/database.go
+++ database/database.go
@@ -11,7 +11,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	dsn := "host=localhost user=charityadmin password=securepassword dbname=charitydb port=5432 sslmode=disable"
+	dsn := "host=localhost user=charityadmin password=anurag#@1353 dbname=charitydb port=5432 sslmode=disable"
 	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
